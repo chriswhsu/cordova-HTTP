5730a1c9305150d39f68ed4ee3523f4d2f26e68d
chriswhsu <chriswhsu@berkeley.edu>
2015-07-09 12:57:15 -0700

    more places to add the new function.

--- src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
@@ -73,6 +73,13 @@ public class CordovaHttpPlugin extends CordovaPlugin {
             HashMap<String, String> headersMap = this.addToMap(this.globalHeaders, headers);
             CordovaHttpPostJson postJson = new CordovaHttpPostJson(urlString, jsonObj, headersMap, callbackContext);
             cordova.getThreadPool().execute(postJson);
+        } else if (action.equals("postJson")) {
+            String urlString = args.getString(0);
+            String jsonObj = args.getString(1);
+            JSONObject headers = args.getJSONObject(2);
+            HashMap<String, String> headersMap = this.addToMap(this.globalHeaders, headers);
+            CordovaHttpPostJsonArray postJsonArray = new CordovaHttpPostJsonArray(urlString, jsonObj, headersMap, callbackContext);
+            cordova.getThreadPool().execute(postJsonArray);
         } else if (action.equals("useBasicAuth")) {
             String username = args.getString(0);
             String password = args.getString(1);
